341696be9abb566e2f8598b34d17ba6e6e71c7dd
Williannca85 <williannca@hotmail.com>
2025-06-04 11:28:00 -0300

    criação de classes

---
+++ entity/conteudo.py
@@ -0,0 +1,16 @@
+""" 
+Atributos: __id_conteudo, __nome_conteudo, __interacoes (lista de objetos Interacao).
+
+Métodos:
+
+adicionar_interacao(), calcular_total_interacoes_engajamento().
+
+calcular_tempo_total_consumo(), listar_comentarios(), etc.
+
+Subclasses de Conteudo (Herança/Polimorfismo):
+
+Video: Método calcular_percentual_medio_assistido().
+
+Podcast e Artigo: Atributos específicos de duração.
+
+"""
---
+++ entity/interacao.py
@@ -0,0 +1,5 @@
+""" 
+Atributos: _conteudo_associado, _plataforma_interacao, _tipo_interacao (validado contra TIPOS_INTERACAO_VALIDOS).
+
+Conversões: timestamp_interacao para datetime, watch_duration_seconds para int.
+"""
